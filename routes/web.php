fbfaef77929981b894e9c01952d3fc946354bf46
shun <amosiknuhs0315@gmail.com>
2022-02-09 19:46:43 +0900

    メール認証機能（途中）

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,30 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Foundation\Auth\EmailVerificationRequest;
+
+// Route::get('/', function () {
+//     return view('app');
+// })->name('/');
 
 Route::get('/{any}', function () {
     return view('app');
 })->where('any', '.*');
 
-// ユーザー登録時メール確認機能
-Route::get('/email/verify', function () {
-    // return view('auth.verify-email');
-})->middleware('auth')->name('verification.notice');
+// Route::get('/', function () {
+//     return view('app');
+// })->name('top');
 
-Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
-    $request->fulfill();
-    return redirect('/home');
-})->middleware(['auth', 'signed'])->name('verification.verify');
+// // ユーザー登録時メール確認機能
+
+// // 新規登録後のメール確認依頼画面（不要予定）
+// Route::get('/email/verify', function () {
+//     // return view('auth.verify-email');
+// })->middleware('auth')->name('verification.notice');
+
+// // 送られてきたメール確認リンクをクリックした後の挙動
+// use Illuminate\Foundation\Auth\EmailVerificationRequest;
+
+// Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+//     $request->fulfill();
+//     return redirect('/');
+// })->middleware(['auth', 'signed'])->name('verification.verify');
