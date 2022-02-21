b7f230d8a2c9a392cafa7424c5fc99c553f46656
shun0315 <amosiknuhs0315@gmail.com>
2022-02-21 14:47:00 +0900

    お気に入り登録機能のテストケース作成

---
+++ tests/Feature/FavoriteTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Database\Seeders\AreasTableSeeder;
+use Database\Seeders\GenresTableSeeder;
+use Database\Seeders\ShopsTableSeeder;
+use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class FavoriteTest extends TestCase
+{
+    use RefreshDatabase;
+    use WithoutMiddleware;
+
+    public function testFavorite()
+    {
+        $this->seed(AreasTableSeeder::class);
+        $this->seed(GenresTableSeeder::class);
+        $this->seed(ShopsTableSeeder::class);
+
+        $this->user = User::factory()->create(
+            [
+                'name' => 'unit_test_user',
+                'email' => 'unit_user@test.com',
+                'password' => Hash::make('password')
+            ]
+        );
+        $this->post('/api/user/login', [
+            'email' => $this->user->email,
+            'password' => 'password'
+        ]);
+
+        $this->post('/api/favorite', [
+            'isFavorite' => 0,
+            'shopId' => 1,
+        ]);
+
+        $this->assertDatabaseHas('favorites', [
+            'user_id' => 1,
+            'shop_id' => 1,
+        ]);
+
+        $this->post('/api/favorite', [
+            'isFavorite' => 1,
+            'shopId' => 1,
+        ]);
+
+        $this->assertDatabaseMissing('favorites', [
+            'user_id' => 1,
+            'shop_id' => 1,
+        ]);
+    }
+}
